5cbcf92b0668b0e1ea4a7f2ffa70284519ae5c88
KevinAlmada <kevinalmada81@gmail.com>
2021-12-29 17:02:23 -0300

    upload images

--- src/Components/CardContainer.jsx
+++ src/Components/CardContainer.jsx
@@ -3,10 +3,10 @@ import CardProjects from './CardProjects'
 import styled from '@emotion/styled'
 
 const proyectos = [
-    {title:"KDDS",description:"E-commerce de una optica",img:"/img/descarga (2).png",url:"https://github.com/KevinAlmada/grupo_1_KDDS"},
-    {title:"Servitronic",description:"Pagina que ofrece servicios",img:"/img/descarga (2).png",url:"https://servitronic.com.ar"},
-    {title:"Seguros",description:"Cotizador de seguros",img:"/img/descarga (2).png",url:"https://compassionate-wiles-f99f0c.netlify.app"},
-    {title:"Veterinaria",description:"Administrador de turnos",img:"/img/descarga (2).png",url:"https://focused-allen-cda5fc.netlify.app"}]
+    {title:"KDDS",description:"E-commerce de una optica",img:"/img/kdds.png",url:"https://github.com/KevinAlmada/grupo_1_KDDS"},
+    {title:"Servitronic",description:"Pagina que ofrece servicios",img:"/img/servitronic.png",url:"https://servitronic.com.ar"},
+    {title:"Seguros",description:"Cotizador de seguros",img:"/img/seguros.png",url:"https://compassionate-wiles-f99f0c.netlify.app"},
+    {title:"Veterinaria",description:"Administrador de turnos",img:"/img/veterinaria.png",url:"https://focused-allen-cda5fc.netlify.app"}]
 const CardContainer = () => {
     const Container = styled.div`
     @media screen and (min-width:678px) {
