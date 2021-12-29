fb1bc0d68692ffe76633a9d4a00ea436c3b53483
KevinAlmada <kevinalmada81@gmail.com>
2021-12-29 17:06:32 -0300

    p styled

--- src/Components/AboutMeInfo.jsx
+++ src/Components/AboutMeInfo.jsx
@@ -4,12 +4,15 @@ const AboutMeInfoStyled = styled.div`
 width: 300px;
 margin-bottom: 100px;
 `;
+const PStyled = styled.p`
+margin-top:20px;
+`;
 const AboutMeInfo = () => {
     return (
         <AboutMeInfoStyled>
             <h2>ABOUT ME</h2>
             <hr />
-            <p>Hi stranger, my name is Kevin Ivan Almada (K.I.A.), im a web developer from Buenos Aires Argentina, I love videogames ,series and programming.</p>
+            <PStyled>Hi stranger, my name is Kevin Ivan Almada (K.I.A.), im a web developer from Buenos Aires Argentina, I love videogames ,series and programming.</PStyled>
         </AboutMeInfoStyled>
     )
 }
