3a0d8dfbe4024c445a453b8b7f6333ee9d6429eb
KevinAlmada <kevinalmada81@gmail.com>
2021-12-30 16:17:25 -0300

    toTop mobile

--- src/Components/ToTop.jsx
+++ src/Components/ToTop.jsx
@@ -9,14 +9,20 @@ const Arrow = styled.a`
     position: fixed;
     width: 50px;
     height: 50px;
-    bottom: 50px;
-    right: 50px;
+    bottom: 40px;
+    right: 10px;
     text-decoration: none;
     color: #FEC260;
     background-color:#2A0944 ;
     border : 3px solid #3B185F ;
     transition: all ease 0.5s;
     opacity: 0;
+    @media screen and (min-width:678px) {
+        width: 60px;
+        height: 60px;
+        bottom: 50px;
+        right: 50px;
+    };
 `;
 const ToTop = () => {
     
